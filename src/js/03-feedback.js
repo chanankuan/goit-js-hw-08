3a47d3692e410ae521f288c7f6447a58bceaa64f
Anton Chan <106675165+chanchik96@users.noreply.github.com>
2023-09-17 16:54:26 +0300

    Task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,26 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const KEY_LS = 'feedback-form-state';
+
+const userData = JSON.parse(localStorage.getItem(KEY_LS)) || {};
+feedbackForm.elements[0].value = userData.email || '';
+feedbackForm.elements[1].value = userData.message || '';
+
+const handleInput = function (event) {
+  const input = event.target;
+
+  userData[input.name] = input.value;
+  localStorage.setItem(KEY_LS, JSON.stringify(userData));
+};
+
+const onSubmit = function (event) {
+  event.preventDefault();
+
+  localStorage.removeItem(KEY_LS);
+  feedbackForm.reset();
+  console.log(userData);
+};
+
+feedbackForm.addEventListener('input', throttle(handleInput, 500));
+feedbackForm.addEventListener('submit', onSubmit);
