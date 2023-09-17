03e8339ec5377f491d4ed6623f3aae8f61379d6e
Anton Chan <106675165+chanchik96@users.noreply.github.com>
2023-09-17 15:51:21 +0300

    Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+console.log(gallery);
+
+const renderImages = arr => {
+  const markup = galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              alt="${description}"
+            />
+          </a>
+        </li>
+        `
+    )
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+};
+renderImages(galleryItems);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
