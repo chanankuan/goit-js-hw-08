33672e83ebc96322471398c13bc68ac5cd724d47
Anton Chan <106675165+chanchik96@users.noreply.github.com>
2023-09-17 15:51:27 +0300

    Task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const KEY_LS = 'videoplayer-current-time';
+
+const onPlay = function (data) {
+  localStorage.setItem(KEY_LS, JSON.stringify(data.seconds));
+};
+
+const getCurrentTime = function () {
+  return JSON.parse(localStorage.getItem(KEY_LS));
+};
+
+player.setCurrentTime(getCurrentTime());
+player.on('timeupdate', throttle(onPlay, 1000));
